a49ed9c1453a5b824f6239e0428b794035f1cd83
Binz-collab <singriet0824@hs.ac.kr>
2025-12-03 03:32:27 +0900

    chore: unit test 환경설정
    react testing library + jest를
    활용해서 단위 테스트를 진행하기 위해
    필요한 라이브러리 설치 및 환경설정
    진행하였습니다.
    
    #4

---
+++ frontend/jest.config.cjs
@@ -0,0 +1,18 @@
+// jest.config.js
+module.exports = {
+  // 테스트 환경을 브라우저와 유사한 jsdom으로 설정
+  testEnvironment: 'jsdom',
+
+  // 각 테스트 파일이 실행되기 전에 실행할 스크립트 파일 지정
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+
+  // CSS, SASS 등의 파일을 만났을 때 에러가 나지 않도록 모의(mock) 처리
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+
+  // .js, .jsx 파일을 babel-jest를 사용해 변환하도록 설정
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
