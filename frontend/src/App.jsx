8e6e691b96c3fb79b0df2098ae5070bed5e02f82
Binz <singriet0824@hs.ac.kr>
2026-01-11 18:01:38 +0000

    refactor:  index 페이지
    기존에 불필요한 컴포넌트화 및 navigate 훅을 사용하지 않는 라우팅 등으로 복잡했던 코드를 병합 및 수정함
    
    #12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
 // import PrivateRoute from "./components/PrivateRoute"
 
 // ✅ 상세 페이지 컴포넌트
 import Project from "./pages/Project.jsx";
 import Home from './pages/Home.jsx'
-import Page from './pages/Page.jsx'
+import Page from './pages/Landing.jsx'
 import './App.css'
-
-
-function NavigationSetter() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // 리액트의 navigate 함수를 윈도우 전역 변수에 연결!
-    window.REACT_APP_NAVIGATE = navigate;
-  }, [navigate]);
-
-  return null; // 화면에는 아무것도 안 그립니다.
-}
 
 
 function App() {
@@ -27,7 +13,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <NavigationSetter />
         <Routes>
             <Route path="/" element={ <Page /> } />
             <Route path="dashboard" element={ 
