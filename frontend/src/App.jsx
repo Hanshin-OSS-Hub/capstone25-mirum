ee6d41d44ed03b3cfeff9f0e00938d6e70b32f8a
Binz-collab <singriet0824@hs.ac.kr>
2026-01-19 02:27:50 +0900

    style: App.css 임포트 추가
    App.jsx에 App.css import 구문을 추가했습니다

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 // import PrivateRoute from "./components/PrivateRoute"
 import AppRoutes from './utils/AppRoutes.jsx'
+import './App.css'
 
 export default function App() {
 
