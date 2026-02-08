5b5de0d9365002cd2d37b4f9c4fb696ffc1ff8c9
Binz-collab <singriet0824@hs.ac.kr>
2026-02-08 14:13:21 +0900

    chore: 패키지 재설치

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import { BrowserRouter } from "react-router-dom"
 // import PrivateRoute from "./components/PrivateRoute"
 import AppRoutes from './utils/AppRoutes.jsx'
 import './App.css'
-import TempProject from './임시/Project..jsx'
 
 export default function App() {
 
   return (
     <>
       <BrowserRouter>
-        {/* <AppRoutes /> */}
-        <TempProject />
+         <AppRoutes />
       </BrowserRouter>
     </>
   )
