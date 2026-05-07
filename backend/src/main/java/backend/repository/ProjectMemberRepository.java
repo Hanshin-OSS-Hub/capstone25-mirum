8aac31852130769c2b779241adcf2f7d2a4320e4
DESKTOP-5FU4EFM\Home <huhjhace@gmail.com>
2026-05-07 17:14:42 +0900

    백업용(26.5.7)

--- backend/src/main/java/backend/repository/ProjectMemberRepository.java
+++ backend/src/main/java/backend/repository/ProjectMemberRepository.java
@@ -31,10 +31,7 @@ public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Lo
     @Query("SELECT pm.user.username FROM ProjectMember pm WHERE pm.role = :role AND pm.project.id = :projectId")
     List<String> findLeader(@Param("projectId") Long projectId, @Param("role") ProjectMemberRoleType role);
 
-    //LEADER 찾는 매서드
-    @Query("SELECT pm FROM ProjectMember pm JOIN FETCH pm.user WHERE pm.project.id = :projectId AND pm.role = :role")
-    Optional<ProjectMember> findLeader(@Param("projectId") Long projectId,
-                                       @Param("role") ProjectMemberRoleType role);
+
 
      Optional<ProjectMember> findByProjectIdAndRole(Long projectId, ProjectMemberRoleType role);
 }
